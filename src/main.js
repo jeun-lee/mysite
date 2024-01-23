dc60e26a698311b5e69a1f248f97df4502138c97
Jeun Lee <jee.u0613@gmail.com>
2024-01-23 13:47:40 +0900

    JS-header

---
+++ src/main.js
@@ -0,0 +1,34 @@
+// Header에 페이지 아래로 스크롤시 다크 스타일링 적용
+const header = document.querySelector('.header');
+const headerHeight = header.getBoundingClientRect().height;
+document.addEventListener('scroll', function(){
+    if(window.scrollY > headerHeight){
+        header.classList.add('header--dark');
+    } else{
+        header.classList.remove('header--dark');
+    }
+});
+// Home 섹션을 아래로 스크롤시 투명하게 처리함
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', function(){
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Arrow-up을 아래로 스크롤시 나타나게 처리함
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', function(){
+    if(window.scrollY > homeHeight / 2){
+        arrowUp.style.opacity = 1;
+    } else {
+        arrowUp.style.opacity = 0;
+    }
+});
+
+// Navbar 토글버튼 클릭 처리
+ const navbarMenu = document.querySelector('.header__menu');
+ const navbarToggle = document.querySelector('.header__toggle');
+ navbarToggle.addEventListener('click', function(){
+    navbarMenu.classList.toggle('open');
+ });
+
